05aad6f5d614e5d787235188f0a3e18139a2b0bb
Adam Koprowski <adamkoprowski4@gmail.com>
2021-05-10 21:40:52 +0200

    add description

--- task4_2.py
+++ task4_2.py
@@ -1,4 +1,9 @@
 def palindrome(str):
+    """
+        Checks if a word is a palindrome
+        Prints answer
+    """
+        
     for i in range(0, int(len(str)/2)):
         if str[i] != str[len(str)-i-1]:
             return False
