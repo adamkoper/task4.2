89391d6b96ce9bfd5f9c796aa479a57fc30ee42b
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-06 15:55:55 +0200

    delete wrong staff

--- task4_2.py
+++ task4_2.py
@@ -5,21 +5,13 @@ def palindrome(str):
         Checks if a word is a palindrome
         Prints answer
     """
-<<<<<<< HEAD
+
     
     string.lower().replace(' ','')[::-1]
     if string.lower().replace(' ','') == string.lower().replace(' ','')[::-1]:
         return True
     else:
         return False
-=======
-        
-    for i in range(0, int(len(str)/2)):
-        if str[i] != str[len(str)-i-1]:
-            return False
-    return True
->>>>>>> main
-
 
  
 if palindrome(string):
